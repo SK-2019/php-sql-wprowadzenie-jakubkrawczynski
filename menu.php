a0a2734030fe981836ac6a126affd5329fe42c79
jakubkrawczynski <jakub.krawczynski2701@gmail.com>
2021-05-17 20:33:52 +0200

    .

---
+++ menu.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jakub Krawczyński</title>
+</head>
+<body>
+<div class="container">
+    <div class="heading">Jakub Krawczyński nr18</div>
+    <div class="menu">
+        <a href="">A</a>
+        <a href="">B</a>
+        <a href="">C</a>
+        <a href="">D</a>
+        <a href="">E</a>
+        <a href="">F</a>
+    </div>
+    <div class="php">
+</body>
+</html>
