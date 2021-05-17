453aa902a931335c4fa7dd9ab0c7fdbfc1cda3c5
jakubkrawczynski <jakub.krawczynski2701@gmail.com>
2021-05-17 20:42:03 +0200

    .

--- connect.php
+++ connect.php
@@ -4,10 +4,10 @@ if ($hostname == 'localhost') {
     require_once ("config.php");
 }
 
-$servername = $_SERVER['servername'];
-$username = $_SERVER['username'];
-$password = $_SERVER['password'];
-$dbname = $_SERVER['dbname'];
+// $servername = $_SERVER['servername'];
+// $username = $_SERVER['username'];
+// $password = $_SERVER['password'];
+// $dbname = $_SERVER['dbname'];
 
 $conn=new mysqli($servername, $username, $password, $dbname);
         if($conn->connect_error) {
